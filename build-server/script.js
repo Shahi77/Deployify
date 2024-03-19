2b3634d5ba7fab08b5179467f3de7449865eae10
Shahi77 <shahiinfinity02@gmail.com>
2024-03-19 19:23:54 +0530

    scripting for build-server

--- build-server/script.js
+++ build-server/script.js
@@ -1,1 +1,25 @@
+const { exec } = require("child_process");
+const path = require("path");
 
+async function init() {
+  console.log("Executing script.js");
+  const outDirPath = path.join(__dirname, "output");
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+  p.stdout.on("data", function (data) {
+    console.log(data.toString());
+  });
+  p.stdout.on("error", function (data) {
+    console.log("Error", data.toString());
+  });
+  p.on("close", function () {
+    console.log("Build complete");
+    const distFolderPath = path.join(__dirname, "output", "dist");
+    const distFolderContents = fs.readdirSync(distFolderPath, {
+      recursive: true,
+    });
+    for (const filePath of distFolderContents) {
+      if (fs.lstatSync(filePath).isDirectory()) continue;
+    }
+  });
+}
